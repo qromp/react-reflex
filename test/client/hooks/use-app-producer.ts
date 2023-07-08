d23efa1a133f4138d6e693616ee34bd633414d96
qromp <34507786+qromp@users.noreply.github.com>
2023-07-08 18:45:57 +0900

    React

--- test/client/hooks/use-app-producer.ts
+++
@@ -1,4 +0,0 @@
-import { UseProducerHook, useProducer } from "@rbxts/roact-reflex";
-import { RootProducer } from "../producer";
-
-export const useAppProducer: UseProducerHook<RootProducer> = useProducer;
